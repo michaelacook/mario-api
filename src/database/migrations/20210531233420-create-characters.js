c9a32757381f8917043828d59a663503f0c6d19a
Michael A. Cook <mcook0775@gmail.com>
2021-05-31 19:41:54 -0400

    Add characters migration

---
+++ src/database/migrations/20210531233420-create-characters.js
@@ -0,0 +1,35 @@
+"use strict"
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface("Characters", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true, 
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      year_released: {
+        type: Sequelize.INTEGER,
+      },
+      debut_game: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Characters")
+  }
+}
