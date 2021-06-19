d97e2fc312a1631ceb6eed6af59f6c918e3288bc
Michael A. Cook <mcook0775@gmail.com>
2021-06-19 16:05:51 -0400

    add global validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import { ValidationPipe } from "@nestjs/common"
 
 import * as dotenv from "dotenv"
 dotenv.config({ path: __dirname + "/.env" })
@@ -7,6 +8,7 @@ dotenv.config({ path: __dirname + "/.env" })
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT || 5000)
 }
 bootstrap()
