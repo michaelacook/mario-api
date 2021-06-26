617df96d8344423f6255f2eb5d432127f128b7a0
Michael A. Cook <mcook0775@gmail.com>
2021-06-26 13:54:18 -0400

    add types

--- src/modules/character/interfaces/character.interface.ts
+++ src/modules/character/interfaces/character.interface.ts
@@ -1,6 +1,6 @@
 import { IDataStoreService } from "src/core/interfaces/data-store-service.interface"
 
 export interface ICharacterService extends IDataStoreService {
-  getAssociatedGames(id): Promise<any>
-  getImage(id): Promise<any>
+  getAssociatedGames(id: number): Promise<any>
+  getImage(id: number): Promise<any>
 }
